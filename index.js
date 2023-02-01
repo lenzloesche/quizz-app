030f5a4f129b9e8cccdc8ff172e3e39f54126193
Lenz Lösche <lenzloesche@gmail.com>
2023-02-01 11:27:40 +0100

    add darkmode half baked

---
+++ index.js
@@ -0,0 +1,17 @@
+const body = document.querySelector('[data-js="body"]');
+const card = document.querySelector('[data-js="card"]');
+const header = document.querySelector('[data-js="header"]');
+const footer = document.querySelector('[data-js="footer"]');
+
+let darkmodeOn = sessionStorage.getItem("darkModeOn");
+
+function changeTheme() {
+  body.classList.toggle("body--dark-mode");
+  card.classList.toggle("card--dark-mode");
+  header.classList.toggle("card--dark-mode");
+  footer.classList.toggle("card--dark-mode");
+}
+if (darkmodeOn == "1") {
+  changeTheme();
+  darkModeSwitch.src = "/images/switch_on.svg";
+}
